da5fe2e44c34a2be574180605c9bbebc4ef843c8
blkdem <blkdem@blkdem.ru>
2026-07-21 14:51:37 +0700

    feat: inline video/audio player for Android
    - Replace native stub with video_player-based inline player
    - Video: play/pause toggle button, duration indicator, black background
    - Audio: play/pause with progress bar, scrubbing support
    - Listener-driven UI updates during playback

--- client/lib/core/utils/html_media_player_native.dart
+++ client/lib/core/utils/html_media_player_native.dart
@@ -1,47 +1,234 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 Widget buildVideoPlayerImpl(String url, {double height = 200}) {
-  return Container(
-    height: height,
-    decoration: BoxDecoration(
-      color: Colors.black12,
-      borderRadius: BorderRadius.circular(8),
-    ),
-    child: Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Icon(Icons.videocam, size: 32),
-          const SizedBox(height: 8),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Tap to play in browser'),
-          ),
-        ],
-      ),
-    ),
-  );
+  return _InlineVideoPlayer(url: url, height: height);
 }
 
 Widget buildAudioPlayerImpl(String url) {
-  return Container(
-    height: 48,
-    decoration: BoxDecoration(
-      color: Colors.black12,
-      borderRadius: BorderRadius.circular(8),
-    ),
-    child: Center(
+  return _InlineAudioPlayer(url: url);
+}
+
+class _InlineVideoPlayer extends StatefulWidget {
+  final String url;
+  final double height;
+
+  const _InlineVideoPlayer({required this.url, required this.height});
+
+  @override
+  State<_InlineVideoPlayer> createState() => _InlineVideoPlayerState();
+}
+
+class _InlineVideoPlayerState extends State<_InlineVideoPlayer> {
+  late VideoPlayerController _controller;
+  bool _initialized = false;
+  bool _hasError = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.url))
+      ..initialize().then((_) {
+        if (mounted) setState(() => _initialized = true);
+      }).catchError((_) {
+        if (mounted) setState(() => _hasError = true);
+      })
+      ..addListener(() {
+        if (mounted) setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_hasError) {
+      return Container(
+        height: widget.height,
+        decoration: BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Center(
+          child: Icon(Icons.broken_image, size: 32, color: Colors.red),
+        ),
+      );
+    }
+
+    if (!_initialized) {
+      return Container(
+        height: widget.height,
+        decoration: BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Center(child: CircularProgressIndicator(strokeWidth: 2)),
+      );
+    }
+
+    return Container(
+      height: widget.height,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            ),
+            GestureDetector(
+              onTap: () {
+                _controller.value.isPlaying
+                    ? _controller.pause()
+                    : _controller.play();
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.black45,
+                  shape: BoxShape.circle,
+                ),
+                padding: const EdgeInsets.all(12),
+                child: Icon(
+                  _controller.value.isPlaying
+                      ? Icons.pause
+                      : Icons.play_arrow,
+                  color: Colors.white,
+                  size: 36,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 4,
+              right: 4,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                decoration: BoxDecoration(
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Text(
+                  _formatDuration(_controller.value.position),
+                  style: const TextStyle(color: Colors.white, fontSize: 10),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _formatDuration(Duration d) {
+    final minutes = d.inMinutes.remainder(60).toString().padLeft(2, '0');
+    final seconds = d.inSeconds.remainder(60).toString().padLeft(2, '0');
+    return '$minutes:$seconds';
+  }
+}
+
+class _InlineAudioPlayer extends StatefulWidget {
+  final String url;
+
+  const _InlineAudioPlayer({required this.url});
+
+  @override
+  State<_InlineAudioPlayer> createState() => _InlineAudioPlayerState();
+}
+
+class _InlineAudioPlayerState extends State<_InlineAudioPlayer> {
+  late VideoPlayerController _controller;
+  bool _initialized = false;
+  bool _hasError = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.url))
+      ..initialize().then((_) {
+        if (mounted) setState(() => _initialized = true);
+      }).catchError((_) {
+        if (mounted) setState(() => _hasError = true);
+      })
+      ..addListener(() {
+        if (mounted) setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_hasError) {
+      return Container(
+        height: 48,
+        decoration: BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Center(
+          child: Icon(Icons.error_outline, size: 20, color: Colors.red),
+        ),
+      );
+    }
+
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      height: 48,
+      decoration: BoxDecoration(
+        color: colorScheme.surfaceContainerHighest.withValues(alpha: 0.5),
+        borderRadius: BorderRadius.circular(8),
+      ),
       child: Row(
-        mainAxisSize: MainAxisSize.min,
         children: [
-          const Icon(Icons.mic, size: 20),
-          const SizedBox(width: 8),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Tap to play in browser'),
+          IconButton(
+            icon: Icon(
+              _initialized && _controller.value.isPlaying
+                  ? Icons.pause_circle_filled
+                  : Icons.play_circle_filled,
+              size: 32,
+            ),
+            color: colorScheme.primary,
+            onPressed: _initialized
+                ? () {
+                    _controller.value.isPlaying
+                        ? _controller.pause()
+                        : _controller.play();
+                  }
+                : null,
           ),
+          if (_initialized)
+            Expanded(
+              child: VideoProgressIndicator(
+                _controller,
+                allowScrubbing: true,
+                colors: VideoProgressColors(
+                  playedColor: colorScheme.primary,
+                  bufferedColor: colorScheme.primary.withValues(alpha: 0.3),
+                  backgroundColor: colorScheme.surfaceContainerHighest,
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+              ),
+            )
+          else
+            const Expanded(
+              child: Center(child: CircularProgressIndicator(strokeWidth: 2)),
+            ),
+          const SizedBox(width: 8),
         ],
       ),
-    ),
-  );
+    );
+  }
 }
